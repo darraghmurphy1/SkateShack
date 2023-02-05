6fc3868d7c56024da10dde0aed16c10255b67c9b
darraghmurphy1 <darraghmurphy77@gmail.com>
2023-02-05 13:03:44 +0000

    changed models to just include category and product

--- products/models.py
+++ products/models.py
@@ -7,53 +7,20 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class Skateboard(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.FloatField()
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(
+        max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
     description = models.TextField()
-    image = models.ImageField(upload_to='skateboard_images/')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(
+        max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-class SkateboardDeck(models.Model):
-    skateboard = models.ForeignKey(Skateboard, on_delete=models.CASCADE)
-    material = models.CharField(max_length=100)
-    width = models.FloatField()
-    length = models.FloatField()
-    image = models.ImageField(upload_to='deck_images/')
-
-    def __str__(self):
-        return f"{self.skateboard} - Deck"
-
-class SkateboardTruck(models.Model):
-    skateboard = models.ForeignKey(Skateboard, on_delete=models.CASCADE)
-    brand = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-    width = models.FloatField()
-    image = models.ImageField(upload_to='truck_images/')
-
-    def __str__(self):
-        return f"{self.skateboard} - Truck"
-
-class SkateboardWheels(models.Model):
-    skateboard = models.ForeignKey(Skateboard, on_delete=models.CASCADE)
-    brand = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-    width = models.FloatField()
-    image = models.ImageField(upload_to='wheels_images/')
-
-    def __str__(self):
-        return f"{self.skateboard} - Wheels"
-
-class SkateboardHardware(models.Model):
-    skateboard = models.ForeignKey(Skateboard, on_delete=models.CASCADE)
-    brand = models.CharField(max_length=100)
-    type = models.CharField(max_length=100)
-    size = models.CharField(max_length=100)
-    color = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='hardware_images/')
-
-    def __str__(self):
-        return self.brand + ' ' + self.type + ' ' + self.size + ' ' + self.color
