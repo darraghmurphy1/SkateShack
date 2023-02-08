7b18c87ae399e8647e964092a61ef6378eff02d6
darraghmurphy1 <darraghmurphy77@gmail.com>
2023-02-08 19:30:09 +0000

    admin updated as well as urls created and edited fror products. views created for product list and product detail.

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
-from .views import product_list, category_list
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('products/', product_list, name='product-list'),
-    path('categories/', category_list, name='category-list'),
+    path('', views.product_list, name='products'),
+    path('<int:product_id>', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,14 +1,31 @@
-from django.shortcuts import render
-from django.core import serializers
-import json
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+
+from .models import Product, Category
 # Create your views here.
 
-def product_list(request):
-    products = Product.objects.all()
-    json_products = serializers.serialize('json', products)
-    return HttpResponse(json_products, content_type='application/json')
 
-def category_list(request):
-    categories = Category.objects.all()
-    json_categories = serializers.serialize('json', categories)
-    return HttpResponse(json_categories, content_type='application/json')
+def product_list(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+    
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
