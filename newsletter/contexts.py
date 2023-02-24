1aa41b4f433d5577b2bf9f5c9f7c794a02b368a2
darraghmurphy1 <darraghmurphy77@gmail.com>
2023-02-24 09:29:33 +0000

    created the app for a newsletter with a small model, form, contexts and admin. displayed in created view

---
+++ newsletter/contexts.py
@@ -0,0 +1,13 @@
+from .forms import SubscriberForm
+
+
+def render_subscribe_form(request):
+    """ Render subscribe form across all pages """
+
+    subscribe_form = SubscriberForm()
+
+    context = {
+        'subscribe_form': subscribe_form,
+    }
+
+    return context
