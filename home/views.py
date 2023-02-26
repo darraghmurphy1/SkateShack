14a2fb00fe4764355cd791562782f03eec2fa387
darraghmurphy1 <darraghmurphy77@gmail.com>
2023-02-26 16:56:05 +0000

    css and layout updates

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     """ A view to display index page """
 
     return render(request, 'home/index.html')
+
+
+def error(request):
+    """ A view to display index page """
+
+    return render(request, 'templates/errors/404.html')
