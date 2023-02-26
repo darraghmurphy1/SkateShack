cdd2f753990105d07d0cc23eda6958a6e8690b13
darraghmurphy1 <darraghmurphy77@gmail.com>
2023-02-26 17:14:51 +0000

    secret key and debug updated

--- skateshack/settings.py
+++ skateshack/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8zup=ho7e%1(7hy9wqwtsk4_j_b22j%maou807z=+arhc!5741'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['skateshack.herokuapp.com', 'localhost']
 
